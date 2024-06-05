09c4c57b0156c16cb4d805a4c9550aec0ad795bc
Matej Straka <strakammm@gmail.com>
2024-06-05 23:48:52 +0200

    wrapping env and testing run

--- generals/__init__.py
+++ generals/__init__.py
@@ -2,7 +2,7 @@
 
 
 
-__all__ = ['grid']
+__all__ = ['GridConfig', 'Grid', 'Generals', 'generals_v0']
 
 __version__ = '0.1.0'
 __author__ = 'Matej Straka'
--- generals/env.py
+++ generals/env.py
@@ -4,22 +4,25 @@ import gymnasium
 from gymnasium.spaces import Discrete
 
 import pettingzoo
-from pettingzoo import ParallelEnv
-from pettingzoo.utils import parallel_to_aec, wrappers
+from pettingzoo.utils import wrappers
 
 
 
-def env(render_mode=None):
-    pass
+def generals_v0(render_mode=None):
+    """
+    Here we apply wrappers to the environment.
+    """
+    env = Generals(num_players=2, map_size=10, max_turns=100, observation_type="numpy")
+    # Apply parallel_to_aec to support AEC api
+    env = pettingzoo.utils.parallel_to_aec(env)
+    return env
 
-def raw_env(render_mode=None):
-    pass
 
-
-class Generals(ParallelEnv):
+class Generals(pettingzoo.ParallelEnv):
     metadata = {'render.modes': ['human', 'rgb_array']}
 
     def __init__(self, num_players, map_size, max_turns, observation_type, debug=False):
+        print('inittin')
         pass
 
     def render(self, mode='human'):
@@ -39,3 +42,5 @@ class Generals(ParallelEnv):
 
     def seed(self, seed=None):
         pass
+
+
