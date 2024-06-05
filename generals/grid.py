cea840985f09893bdb54eca4057ef391fa2ae69c
Matej Straka <strakammm@gmail.com>
2024-06-05 22:14:12 +0200

    first skeletons

--- generals/grid.py
+++ generals/grid.py
@@ -1,9 +1,28 @@
 
 
 import numpy as np
-from .grid_config import GridConfig
+from grid_config import GridConfig
 
 
 class Grid():
     def __init__(self, config: GridConfig):
         self.config = config
+        self.n_channels = 5
+        self.grid = self.generate()
+        print(self.grid[..., 0])
+
+
+    def generate(self):
+        map = np.zeros((self.config.grid_size, self.config.grid_size, self.n_channels), dtype=np.float32)
+        map[self.config.starting_positions[0][0], self.config.starting_positions[0][1], 0] = 1
+        map[self.config.starting_positions[1][0], self.config.starting_positions[1][1], 0] = 1
+        return map
+
+
+config = GridConfig(
+    grid_size=10,
+    starting_positions=[[1, 1], [5, 5]]
+)
+
+g = Grid(config)
+
--- generals/grid_config.py
+++ generals/grid_config.py
@@ -1,7 +1,10 @@
+from typing import List, Tuple
 
+from pydantic import BaseModel, validator, ValidationError
 
-class BaseConfig():
-    pass
-
-class GridConfig(BaseConfig):
-    pass
+class GridConfig(BaseModel):
+    grid_size: int = 32
+    town_density: float = 0.1
+    terrain_density: float = 0.1
+    starting_positions: List[Tuple[int, int]] = [(0, 0), (10, 10)]
+    map_name: str = None
