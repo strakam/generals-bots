1b46f22847ea7fa2e7efa610a960f9e05d9f84df
Matej Straka <strakammm@gmail.com>
2024-06-06 13:07:10 +0200

    drawing first grid

--- generals/utils.py
+++ generals/utils.py
@@ -1,17 +1,26 @@
 import pygame
 
 
-display_width = 800
-display_height = 800
 
 class Visualizer():
-    def __init__(self):
+    square_size = 40
+    def __init__(self, grid_size=20):
         pygame.init()
-        self.screen = pygame.display.set_mode((display_width, display_height))
+        self.grid_size = grid_size
+        self.window_size = self.square_size * grid_size
+        self.screen = pygame.display.set_mode((self.window_size, self.window_size))
         self.clock = pygame.time.Clock()
 
+        self.initial_draw()
+
     def draw_grid(self, grid):
-        self.screen.fill("purple")
         pygame.draw.line(self.screen, "red", (20, 20), (100, 100))
         print('no shit')
         pygame.display.flip()
+
+    def initial_draw(self):
+        self.screen.fill("gray")
+        for i in range(1, self.grid_size):
+            pygame.draw.line(self.screen, "black", (self.square_size*i, 0), (self.square_size*i, self.window_size), 2)
+            pygame.draw.line(self.screen, "black", (0, self.square_size*i), (self.window_size, self.square_size*i), 2)
+
