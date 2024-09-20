19d7e47e6cd9d586bfa01e0f1c7c8e28f98a89a5
Matej Straka <strakammm@gmail.com>
2024-09-20 13:49:08 +0200

    adding tiles to rendering

--- generals/rendering.py
+++ generals/rendering.py
@@ -18,11 +18,10 @@ class Renderer:
         pygame.display.set_caption("Generals")
         pygame.key.set_repeat(500, 64)
 
-
         self.game = game
         self.agents = game.agents
         self.grid_size = game.grid_size
-        self.grid_offset = c.UI_ROW_HEIGHT * (len(self.agents) + 1) # area for GUI
+        self.grid_offset = c.UI_ROW_HEIGHT * (len(self.agents) + 1)  # area for GUI
         self.window_width = max(c.MINIMUM_WINDOW_SIZE, c.SQUARE_SIZE * self.grid_size)
         self.window_height = max(
             c.MINIMUM_WINDOW_SIZE, c.SQUARE_SIZE * self.grid_size + self.grid_offset
@@ -38,8 +37,19 @@ class Renderer:
         )
         # Game area
         self.game_area = pygame.Surface(
-            (self.window_width, self.window_height - c.UI_ROW_HEIGHT * (len(self.agents) + 1))
+            (
+                self.window_width,
+                self.window_height - c.UI_ROW_HEIGHT * (len(self.agents) + 1),
+            )
         )
+        # Tiles
+        self.tiles = [
+            [
+                pygame.Surface((c.SQUARE_SIZE, c.SQUARE_SIZE))
+                for _ in range(self.grid_size)
+            ]
+            for _ in range(self.grid_size)
+        ]
 
         self.agent_fov = {name: True for name in self.agents}
         self.game_speed = 1
@@ -48,12 +58,15 @@ class Renderer:
         self.clock = pygame.time.Clock()
         self.last_render_time = time.time()
 
-        self._mountain_img = pygame.image.load(str(c.MOUNTAIN_PATH), "png").convert_alpha()
-        self._general_img = pygame.image.load(str(c.GENERAL_PATH), "png").convert_alpha()
+        self._mountain_img = pygame.image.load(
+            str(c.MOUNTAIN_PATH), "png"
+        ).convert_alpha()
+        self._general_img = pygame.image.load(
+            str(c.GENERAL_PATH), "png"
+        ).convert_alpha()
         self._city_img = pygame.image.load(str(c.CITY_PATH), "png").convert_alpha()
 
         self._font = pygame.font.Font(c.FONT_PATH, c.FONT_SIZE)
-
 
     def handle_events(self):
         """
@@ -61,21 +74,21 @@ class Renderer:
         """
         agents = self.game.agents
         control_events = {
-            'time_change': 0,
+            "time_change": 0,
         }
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
             if event.type == pygame.KEYDOWN:
                 self.changed = True
-                control_events['changed'] = True
+                control_events["changed"] = True
                 if event.key == pygame.K_q:
                     pygame.quit()
                     quit()
 
                 # Speed up game right arrow is pressed
                 if event.key == pygame.K_RIGHT and not self.paused:
-                    self.game_speed = max(1/16, self.game_speed / 2)
+                    self.game_speed = max(1 / 16, self.game_speed / 2)
                 # Slow down game left arrow is pressed
                 if event.key == pygame.K_LEFT and not self.paused:
                     self.game_speed = min(32, self.game_speed * 2)
@@ -85,10 +98,10 @@ class Renderer:
 
                 # Control replay frames
                 if event.key == pygame.K_h:
-                    control_events['time_change'] = -1
+                    control_events["time_change"] = -1
                     self.paused = True
                 if event.key == pygame.K_l:
-                    control_events['time_change'] = 1
+                    control_events["time_change"] = 1
                     self.paused = True
 
             # GUI clicks
@@ -103,9 +116,6 @@ class Renderer:
         control_events = self.handle_events()
         self.render_grid()
         self.render_gui(from_replay)
-        self.game_area.fill(c.WHITE)
-        self.screen.blit(self.scoreboard, (0, 0))
-        self.screen.blit(self.game_area, (0, self.grid_offset))
         pygame.display.flip()
         return control_events
 
@@ -116,12 +126,8 @@ class Renderer:
         names = self.game.agents
         ids = [self.game.agent_id[name] for name in names]
         player_stats = self.game.get_infos()
-        army_counts = ["Army"] + [
-            str(player_stats[name]["army"]) for name in names
-        ]
-        land_counts = ["Land"] + [
-            str(player_stats[name]["land"]) for name in names
-        ]
+        army_counts = ["Army"] + [str(player_stats[name]["army"]) for name in names]
+        land_counts = ["Land"] + [str(player_stats[name]["land"]) for name in names]
         fovs = ["FOV"] + ["  X" if self.agent_fov[name] else " " for name in names]
         names = ["Turn"] + [name for name in names]
 
@@ -162,16 +168,16 @@ class Renderer:
             )
 
         if from_replay:
-            speed = (
-                "Paused" if self.paused else str(1 /  self.game_speed) + "x"
-            )
+            speed = "Paused" if self.paused else str(1 / self.game_speed) + "x"
             text = self._font.render(f"Game speed: {speed}", True, c.BLACK)
             self.scoreboard.blit(text, (150, 15))
 
         # For each player print his name, army, land and FOV (Field of View)
         for i in range(len(ids) + 1):
             if i == 0:
-                turn = str(self.game.time // 2) + ("." if self.game.time % 2 == 1 else "")
+                turn = str(self.game.time // 2) + (
+                    "." if self.game.time % 2 == 1 else ""
+                )
                 text = self._font.render(f"{names[i]}: {turn}", True, c.BLACK)
             else:
                 text = self._font.render(f"{names[i]}", True, c.BLACK)
@@ -191,10 +197,11 @@ class Renderer:
                 text, (self.window_width - 3 * c.GUI_CELL_WIDTH + x_offset, y_offset)
             )
         self.changed = False
+        self.screen.blit(self.scoreboard, (0, 0))
 
     def render_grid(self):
         """
-        Render grid part of the game.
+        Render grid part of the game on
 
         Args:
             game: Game object
@@ -225,23 +232,8 @@ class Renderer:
         for agent in agents:
             ownership = self.game.channels["ownership_" + agent]
             ownership_indices = self.game.channel_to_indices(ownership)
-            self.draw_channel(ownership_indices, c.PLAYER_COLORS[self.game.agent_id[agent]])
-
-        # draw lines
-        for i in range(1, self.grid_size):
-            pygame.draw.line(
-                self.screen,
-                c.BLACK,
-                (0, i * c.SQUARE_SIZE + self.grid_offset),
-                (self.window_width, i * c.SQUARE_SIZE + self.grid_offset),
-                c.LINE_WIDTH,
-            )
-            pygame.draw.line(
-                self.screen,
-                c.BLACK,
-                (i * c.SQUARE_SIZE, self.grid_offset),
-                (i * c.SQUARE_SIZE, self.window_height),
-                c.LINE_WIDTH,
+            self.draw_channel(
+                ownership_indices, c.PLAYER_COLORS[self.game.agent_id[agent]]
             )
 
         # draw background as squares
@@ -280,7 +272,9 @@ class Renderer:
         visible_cities_neutral = np.logical_and(
             visible_cities, self.game.channels["ownership_neutral"]
         )
-        visible_cities_neutral_indices = self.game.channel_to_indices(visible_cities_neutral)
+        visible_cities_neutral_indices = self.game.channel_to_indices(
+            visible_cities_neutral
+        )
         self.draw_channel(visible_cities_neutral_indices, c.NEUTRAL_CASTLE)
 
         # draw visible city images
@@ -296,13 +290,15 @@ class Renderer:
             x_offset = c.FONT_OFFSETS[
                 min(len(c.FONT_OFFSETS) - 1, len(str(int(army[i, j]))) - 1)
             ]
-            self.screen.blit(
-                text,
-                (
-                    j * c.SQUARE_SIZE + x_offset,
-                    i * c.SQUARE_SIZE + y_offset + self.grid_offset,
-                ),
-            )
+            self.tiles[i][j].blit(text, (x_offset, y_offset))
+
+        # Blit tiles to the self.game_area
+        for i in range(self.grid_size):
+            for j in range(self.grid_size):
+                self.game_area.blit(
+                    self.tiles[i][j], (j * c.SQUARE_SIZE, i * c.SQUARE_SIZE)
+                )
+        self.screen.blit(self.game_area, (0, self.grid_offset))
 
     def draw_channel(self, channel: list[Tuple[int, int]], color: Tuple[int, int, int]):
         """
@@ -312,14 +308,10 @@ class Renderer:
             channel: list of tuples with indices of the channel
             color: color of the squares
         """
-        size, offset = c.SQUARE_SIZE, self.grid_offset
-        w = c.LINE_WIDTH
         for i, j in channel:
-            pygame.draw.rect(
-                self.screen,
-                color,
-                (j * size + w, i * size + w + offset, size - w, size - w),
-            )
+            self.tiles[i][j].fill(color)
+            pygame.draw.line(self.tiles[i][j], c.BLACK, (0, 0), (0, c.SQUARE_SIZE), 1)
+            pygame.draw.line(self.tiles[i][j], c.BLACK, (0, 0), (c.SQUARE_SIZE, 0), 1)
 
     def draw_images(self, channel: list[Tuple[int, int]], image):
         """
@@ -330,6 +322,5 @@ class Renderer:
             channel: list of tuples with indices of the channel
             image: pygame image object
         """
-        size, offset = c.SQUARE_SIZE, self.grid_offset
         for i, j in channel:
-            self.screen.blit(image, (j * size + 3, i * size + 3 + offset))
+            self.tiles[i][j].blit(image, (3, 2))
