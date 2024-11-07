f321b8db78024e36c6ecc5e2297538498d8cdb43
Katerina Vankova <katerinavankova1@gmail.com>
2024-11-07 18:50:24 +0100

    chore: Make changes to example

--- examples/gymnasium_example.py
+++ examples/gymnasium_example.py
@@ -8,10 +8,3 @@ npc = ExpanderAgent()
 
 # Create environment
 env = gym.make("gym-generals-v0", agent=agent, npc=npc, render_mode="human")
-
-observation, info = env.reset()
-terminated = truncated = False
-while not (terminated or truncated):
-    action = agent.act(observation)
-    observation, reward, terminated, truncated, info = env.step(action)
-    env.render()
