27e90618aca28e7dd4222a63aa31ac8e255f4aa4
Matej Straka <strakammm@gmail.com>
2025-01-10 16:21:54 +0100

    docs: Make shorter example

--- examples/pettingzoo_example.py
+++ examples/pettingzoo_example.py
@@ -2,11 +2,8 @@ from generals.agents import RandomAgent, ExpanderAgent
 from generals.envs import PettingZooGenerals
 
 # Initialize agents
-random = RandomAgent()
-expander = ExpanderAgent()
-
-# Names are used for the environment
-agent_names = [random.id, expander.id]
+random = RandomAgent(id="Random")
+expander = ExpanderAgent(id="Expander")
 
 # Store agents in a dictionary
 agents = {
@@ -15,15 +12,14 @@ agents = {
 }
 
 # Create environment
-env = PettingZooGenerals(agent_ids=agent_names, to_render=True)
+env = PettingZooGenerals(agent_ids=[random.id, expander.id], to_render=True)
 observations, info = env.reset()
 
-done = False
-while not done:
+terminated = truncated = False
+while not (terminated or truncated):
     actions = {}
     for agent in env.agent_ids:
         actions[agent] = agents[agent].act(observations[agent])
     # All agents perform their actions
     observations, rewards, terminated, truncated, info = env.step(actions)
-    done = terminated or truncated
     env.render()
