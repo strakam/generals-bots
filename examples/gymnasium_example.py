38a53322eec1a03acdfc0e8b1d08bfddf37503f3
Matej Straka <strakammm@gmail.com>
2025-06-29 16:57:17 +0200

    chore: Update usage example

--- examples/gymnasium_example.py
+++ examples/gymnasium_example.py
@@ -12,29 +12,37 @@ grid_factory = GridFactory(
     max_grid_dims=(24, 24),
 )
 
-n_envs = 12
+# Create n_envs parallel environments
+n_envs = 1
 envs = gym.vector.AsyncVectorEnv(
     [lambda: GymnasiumGenerals(agents=agent_names, grid_factory=grid_factory, truncation=500) for _ in range(n_envs)],
 )
 
 
+# Observations have shape: (n_envs, n_agents, grid_height, grid_width, n_channels)
+# To access observation of the first agent, you can do: observations[:, 0, :, :, :]
+
 observations, infos = envs.reset()
 terminated = [False] * len(observations)
 truncated = [False] * len(observations)
-import time
 
-steps = 0
-start_time = time.time()
-while steps < 100_000:
-    while True:
-        agent_actions = [envs.single_action_space.sample() for _ in range(n_envs)]
-        npc_actions = [envs.single_action_space.sample() for _ in range(n_envs)]
+while True:
+    # Sample random actions for each agent
+    agent_actions = [envs.single_action_space.sample() for _ in range(n_envs)]
+    npc_actions = [envs.single_action_space.sample() for _ in range(n_envs)]
 
-        # Stack actions together
-        actions = np.stack([agent_actions, npc_actions], axis=1)
-        observations, rewards, terminated, truncated, infos = envs.step(actions)
-        masks = [np.stack([infos[agent_name]["masks"] for agent_name in agent_names])]
-        if any(terminated) or any(truncated):
-            break
-        steps += 12
-print("FPS: ", steps / (time.time() - start_time))
+    # Stack actions together
+    actions = np.stack([agent_actions, npc_actions], axis=1)
+    observations, dummy_rewards, terminated, truncated, infos = envs.step(actions)
+
+    # Extract masks for each agent
+    masks = [np.stack([infos[agent_name]["masks"] for agent_name in agent_names])]
+
+    # Since gymnasium support single agent (scalar) rewards, we pack them in 'infos',
+    # where these rewards can be accessed like this:
+    real_rewards = [
+        np.stack([infos[agent_name]["reward"] for agent_name in agent_names])
+    ]
+
+    if any(terminated) or any(truncated):
+        break
